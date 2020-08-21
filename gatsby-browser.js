14fd07105bff1016df741fb8b2de5ed5b0c5bdee
Pedro <pedro@pedrolourenco.net>
2020-08-21 02:56:32 +0100

    Fix flashing huge Font Awesome icons

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,7 @@
 import 'static/fonts.css'
 import 'static/vars.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+// https://github.com/FortAwesome/react-fontawesome/issues/234
+config.autoAddCss = false
