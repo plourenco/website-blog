20440d65896132453663903b4a32fab69ebb9fe0
Pedro <pedro@plourenco.com>
2020-10-08 23:05:02 +0200

    Update note responsiveness

--- src/components/blog/note.js
+++ src/components/blog/note.js
@@ -4,7 +4,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styles from './note.module.scss'
 
 export default function Note({ color, children }) {
-  const _color = ['warning', 'danger'].includes(color) ? color : 'alert'
+  const _color = ['warning', 'danger', 'success'].includes(color)
+    ? color
+    : 'alert'
   return (
     <div className={`${styles[_color]}`}>
       <FontAwesomeIcon icon={faExclamationCircle} />
