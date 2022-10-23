346fe624548456a59f95b8eaec01a7f8ada31648
Pedro Lourenço <public@plourenco.com>
2022-10-23 21:50:46 +0100

    Update about me

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import About from 'components/about';
-import Education from 'components/about/education';
 import Why from 'components/about/why';
 import Layout from 'components/layout';
 
@@ -8,7 +7,6 @@ export default function AboutPage() {
   return (
     <Layout>
       <About />
-      <Education />
       <Why />
     </Layout>
   );
